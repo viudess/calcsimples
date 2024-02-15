87454e3861c6afd69603be865d7f3ceeb7b9c511
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-15 07:33:11 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'calcsimples',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
